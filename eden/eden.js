3af6929d79e59f00e0ef68cca49e40543e4e2c71
Zabolekar <syekirin@gmail.com>
2018-08-08 16:55:16 +0200

    more cleanup

--- eden/eden.js
+++ eden/eden.js
@@ -41,7 +41,7 @@ const leg_raw = [{x:0, y: 40},
                   {x: 10, y: 0},
                   {x:0, y: 0}];
 
-var exponates = d3.range(1, n+1).map(function(i) {
+var exponates = d3.range(1, n+1).map(i => {
    with (Math) {
       function randomize (o) {
          return {x: o.x+5*random(), y: o.y+5*random()};
@@ -51,27 +51,32 @@ var exponates = d3.range(1, n+1).map(function(i) {
           y0 = height * (0.5 + (random()*0.2-0.1)),
           r1 = 20,
           r2 = 80,
-          leftRibs = d3.range(PI/2, 3*PI/2, PI/80).map(function(phi) {
-             return random() > 0.5+(i-1)/(2*n) ? 0 : phi}).map(function(phi) {
-                return {x: max(min(phi == 0 ? 0 : ((i-1)/(2*n)+1)*r2*cos(phi),
-                                   random() > 0.5 ? 80 : 60),
-                               random() > 0.5 ? -80 : -60),
-                        y: ((phi == 0) ? 0 : 1.5*r2*sin(phi)+random())};
-          }),
-          leg = leg_raw.map(function (o) {
-             return {
-                x: (o.x+random()*50*(i-1)/n)/pow(i, 0.3),
-                y: (o.y+random()*50*(i-1)/n)/pow(i, 0.3)+10
-             };
-          }),
-          spine = d3.range(-20,20).map(function(j) {
-             return {x: x0+5*sin(5*j)+random(),
-                   y: y0+j+random()};
-          }),
-          head = d3.range(PI/2, 3*PI/2, PI/20).map(function(phi) {
-             return {x: -15*cos(phi)*(1-(i-1)/(n-1)+(i-1)/(n-1)*(sin(phi)+3)),
-                      y: 15*sin(phi)-20};
-          }),
+          leftRibs = d3.range(PI/2, 3*PI/2, PI/80)
+                       .map(phi => random() > 0.5+(i-1)/(2*n) ? 0 : phi)
+                       .map(phi => 
+                           ({x: max(min(phi == 0 ? 0 : ((i-1)/(2*n)+1)*r2*cos(phi),
+                                        random() > 0.5 ? 80 : 60),
+                                    random() > 0.5 ? -80 : -60),
+                             y: phi == 0 ? 0 : 1.5*r2*sin(phi)+random()})
+                        ),
+          plate = d3.range(0, 2*PI, PI/36).map(phi =>
+             ({x: max(min(r1*cos(phi)*(sin(phi)+4*(i-1)/n),
+                          50),
+                      -50),
+               y: pow(i, 0.2)*r1*sin(phi)})
+          ),
+          leg = leg_raw.map(o =>
+             ({x: (o.x+random()*50*(i-1)/n)/pow(i, 0.3),
+               y: (o.y+random()*50*(i-1)/n)/pow(i, 0.3)+10})
+          ),
+          spine = d3.range(-20,20).map(j =>
+             ({x: 5*sin(5*j)+random(),
+               y: j+random()})
+          ),
+          head = d3.range(PI/2, 3*PI/2, PI/20).map(phi =>
+             ({x: -15*cos(phi)*(1-(i-1)/(n-1)+(i-1)/(n-1)*(sin(phi)+3)),
+               y: 15*sin(phi)-20})
+          ),
           eye = eye_raw.map(randomize),
           hand = hand_raw.map(randomize);
 
@@ -80,29 +85,37 @@ var exponates = d3.range(1, n+1).map(function(i) {
       }
  
       function symmetrize (data) {
-         return data.concat([...data].reverse().map(o => ({x: -o.x, y: o.y}))).map(offset);
+         return data.concat([...data].reverse().map(o => ({x: -o.x, y: o.y})));
       }
 
       return {
-         ribs: symmetrize(leftRibs),
-         plate: d3.range(0, 2*PI, PI/36).map(function(phi){
-            return {x: x0+max(min(r1*cos(phi)*(sin(phi)+4*(i-1)/n), 50), -50),
-                    y: y0+Math.pow(i, 0.2)*r1*sin(phi)};
-         }),
-         legs: symmetrize(leg),
-         spine: spine,
-         hands: symmetrize(hand),
-         head: symmetrize(head),
-         eyes: symmetrize(eye)
+         ribs: symmetrize(leftRibs).map(offset),
+         plate: plate.map(offset),
+         legs: symmetrize(leg).map(offset),
+         spine: spine.map(offset),
+         hands: symmetrize(hand).map(offset),
+         head: symmetrize(head).map(offset),
+         eyes: symmetrize(eye).map(offset)
       };
    }
 });
 
-var svg = d3.select("body #gallery").append("svg").attr("width", width).attr("height", height).on("mousedown", () => { v_x = -100; }).on("mouseup", () => { v_x = -1; });
-var g = svg.selectAll("g").data(exponates).enter().append("g");
+var svg = d3.select("body #gallery")
+            .append("svg")
+            .attr("width", width)
+            .attr("height", height)
+            .on("mousedown", () => v_x = -100)
+            .on("mouseup", () => v_x = -1);
 
-["ribs", "legs", "plate", "spine", "hands", "head", "eyes"].forEach(function (s) {
-   g.append("path").datum(d => d[s]).attr("class", s);
+var g = svg.selectAll("g")
+           .data(exponates)
+           .enter()
+           .append("g");
+
+["ribs", "legs", "plate", "spine", "hands", "head", "eyes"].forEach(s => {
+   g.append("path")
+    .datum(d => d[s])
+    .attr("class", s);
 })
 
 //Moving slowly to the left
@@ -110,9 +123,10 @@ var v_x = -1,
     position = 0, 
     everything = g.selectAll("path");
 
-d3.timer(function(time) {
+d3.timer(time => {
   position += v_x;
-  everything.attr("d", lineFunction).attr("transform", "translate(" + position + ",0)");
+  everything.attr("d", lineFunction)
+            .attr("transform", "translate(" + position + ",0)");
 });
 
 var lineFunction = d3.svg.line().x(d => d.x).y(d => d.y).interpolate("basis");
